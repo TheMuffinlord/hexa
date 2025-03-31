5890ebd832be2921b0ac341e8cac1f184fae29a6
TheMuffinlord <tcgiant@gmail.com>
2025-03-31 18:35:00 -0400

    i'm going about this all wrong

--- hexes.py
+++ hexes.py
@@ -2,22 +2,46 @@ import pygame, math
 from constants import *
 
 class Hexagon(pygame.sprite.Sprite):
-    def __init__(self, row, col):
+    def __init__(self, col, row):
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
-        self.position = pygame.Vector2(row, col)
-        self.size = HEX_SIZE
+        #self.position = pygame.Vector2(row*HEX_SIZE, col*HEX_SIZE)
+        self.size = HEX_SIZE #zoom logic will go here eventually ig
+        self.radius = HEX_SIZE/2
+        #self.half_height = math.sqrt((self.size * 2) - (self.radius*2))
+        #pos_x = col * 
+        self.col = col
+        self.row = row
+        self.position = pygame.Vector2(self.get_x(), self.get_y())
+        self.corners = []
+        for i in range(0,6):
+            self.corners.append(self.hex_corner(i))
+        
+    def hex_width(self):
+        return self.size * math.cos(math.radians(30))
+    
+    def hex_height(self):
+        return self.size * math.sin(math.radians(30))
 
     def hex_corner(self, corner):
         angle_degrees = 60 * corner
         angle_radius = math.pi / 180 * angle_degrees
-        return (self.position.x + self.size * math.cos(angle_radius), self.position.y + self.size * math.sin(angle_radius))
+        return (int(self.position.x + self.size * math.cos(angle_radius)), int(self.position.y + self.size * math.sin(angle_radius)))
+
+    def get_x(self):
+        if self.col % 2 == 0:
+            return int(self.col * self.size*2)
+        return int(self.col * (self.size * 1.5))
+        #return int(self.col * ((math.sqrt(3) * (self.size/2))))
+    
+    def get_y(self):
+        if self.row % 2 == 0:
+            return int(self.row * self.size)
+        return int(self.row * (self.size*0.75))
+        #return int(self.row * (self.hex_height()*2+2))
 
     def draw(self, screen):
-        corners = []
-        for i in range(0,6):
-            corners.append(self.hex_corner(i))
-        pygame.draw.polygon(screen, "white", corners)
+        pygame.draw.polygon(screen, "white", self.corners, 2)
 
--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame, sys
 from constants import *
 from hexes import *
 
+def fill_map():
+    hexmap = []
+    for row in range(MAP_ROWS):
+        #if row % 2 == 1:
+            #
+            # new_y = hex1.radius + (row * HEX_SIZE)
+        #else:
+            #cry i guess
+        for col in range(MAP_COLS):
+            new_hex = Hexagon(col, row)
+            hexmap.append(new_hex)
+
 def main():
     pygame.init()
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -10,15 +22,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     #doing p good not to eat shit at this stage
-    hex1 = Hexagon(100,100)
+   
 
+    hexfield = pygame.sprite.Group()
+
+    Hexagon.containers = (hexfield)
+
+    #hex1 = Hexagon(0,0)
+    #for corner in hex1.corners:
+    #    print(corner)
+    #print(hex1.hex_width())
+    #hex2 = Hexagon(1,1)
+    fill_map()
+    #print(len(hexmap))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        hex1.draw(screen)
-
+        for hexagon in hexfield:
+            hexagon.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
     #fukken idk
